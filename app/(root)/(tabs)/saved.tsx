ae6979f6636101d2ef888036c99af72e342af80f
zainahmad-dev <zainahmad4708@gmail.com>
2026-07-02 16:44:04 +0500

    Minor Changes

--- app/(root)/(tabs)/saved.tsx
+++ app/(root)/(tabs)/saved.tsx
@@ -1,10 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function Saved() {
   return (
-    <View>
-      <Text>Saved</Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-gray-50">
+      <View>
+        <Text>Saved</Text>
+      </View>
+    </SafeAreaView>
   )
 }
